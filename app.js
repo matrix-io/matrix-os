8836f65e185c060a0aa37ad66e7871f77c70b5f7
sc <god@h7n.local>
2015-07-07 13:12:00 -0400

    Progress

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 console.log('-= AdM app=-');
 console.error('error test');
 
-Matrix = require('lib');
+log = require('captains-log')();
+_ = require('lodash');
 
+var events = require('events');
+
+// Core
+Matrix = require('./lib/');
+//Event Loop
+Matrix.events = new events.EventEmitter();
+
+// Example
+/*
+Matrix.events.on('poop', function(data){
+  log('iPooped!',data);
+});
+
+Matrix.events.emit('poop', { stinky: true });
+*/
+
+log(Matrix);
