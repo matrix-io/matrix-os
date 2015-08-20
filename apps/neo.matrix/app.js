4573e4a34c73f59b2fe776d47452642fd01b4ef0
Brian Sanchez <brian@rokk3rlabs.com>
2015-08-20 14:19:01 -0400

    updates to neo.matrix app

--- apps/neo.matrix/app.js
+++ apps/neo.matrix/app.js
@@ -1,24 +1,28 @@
 var mic = matrix.init('mic');
 var audio = matrix.init('audio');
 var controls = matrix.init('controls');
-var file = matrix.init('file');
+var file = matrix.file;
 
 //if file does not exist, throw false
 var name = file.load('name.txt');
+
+//check if name is empty
 if(name) {
   mic.listen(name).then(function(err, out){
       matrix.notify(out);
       //some sort of namespace for each app
   });
 } else {
+  
+  //say that it needs a name
+  audio.say('Please log into your mobile application and give me a name');
 
-  audio.say('Please log into your mobile application and give me a name.');
-
+  //wait for the name to be entered
   controls.on('text.message', function(err, name){
     file.save('name.txt', name);
     mic.listen(name).then(function(err, out){
-        matrix.notify(out);
-        //some sort of namespace for each app
+      //some sort of namespace for each app
+      matrix.notify(out);
     });
   });
 }
