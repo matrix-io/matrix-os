3e6b55167e479b17b831cc57dfc06f18f01bce45
Brian Sanchez <brian@rokk3rlabs.com>
2015-08-20 14:28:27 -0400

    minor changes with comments

--- apps/neo.matrix/app.js
+++ apps/neo.matrix/app.js
@@ -1,24 +1,28 @@
 var mic = matrix.init('mic');
 var audio = matrix.init('audio');
-var controls = matrix.init('controls');
+var controls = matrix.init('controls'); //connect to channel for user
 var file = matrix.file;
 
 //if file does not exist, throw false
 var name = file.load('name.txt');
 
-//check if name is empty
+//check if name is not empty
 if(name) {
+  //if name is not empty, then listen for speech to text, 
+  //and output when "name" is heard
   mic.listen(name).then(function(err, out){
+      if(err) throw err;
       matrix.notify(out);
-      //some sort of namespace for each app
+      //message out on matrix, some sort of namespace for each app
   });
 } else {
-  
+
   //say that it needs a name
   audio.say('Please log into your mobile application and give me a name');
 
   //wait for the name to be entered
   controls.on('text.message', function(err, name){
+    if(err) throw err;
     file.save('name.txt', name);
     mic.listen(name).then(function(err, out){
       //some sort of namespace for each app
@@ -27,7 +31,7 @@ if(name) {
   });
 }
 
-//events coming in
+//listen for events coming in
 matrix.on('neo.say',function(err, out){
     audio.say(out);
 });
