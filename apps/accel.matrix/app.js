14de5b08373eac374747d0de72a08082facc6e23
Sean Canton <eighteyes@gmail.com>
2015-09-11 15:56:42 -0400

    New apps!

---
+++ apps/accel.matrix/app.js
@@ -0,0 +1,28 @@
+
+matrix.init('accel').then(function(err, data){
+  if (err) console.error(err);
+  if (data === false || typeof data === 'undefined' ){
+    console.error('no match on filter'.red);
+  } else {
+    console.log('app(accel):then>'.green, data);
+    matrix.send(data);
+
+    //test specific app + event
+    // matrix.notify('test-event','doTest', data);
+
+    //test specific app
+    // matrix.notify('test-event', {global: false});
+
+    //test global
+    //matrix.notify({global:true});
+  }
+});
+
+matrix.on(function(data){
+  console.log('recieved global message'.blue, data);
+});
+//
+// matrix.file.save('http://google.com', 'google.txt', function(err, result){
+//   if (err) console.error(err);
+//   // console.log(result);
+// });
---
+++ apps/accel.matrix/index.js
@@ -0,0 +1,4 @@
+matrix = require('./../matrix.js');
+matrix.name(require('path').basename(__dirname).split('.')[0]);
+
+require('./app.js');
