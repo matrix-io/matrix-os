5c49b7039383d3ab05b82e02e8ae5c48327d03a0
Brian Sanchez <brian@rokk3rlabs.com>
2015-09-17 10:49:12 -0400

    updated apps

--- apps/neo.matrix/app.js
+++ apps/neo.matrix/app.js
@@ -16,7 +16,7 @@ exports.parseResult = function (err, resp, body) {
     var store = JSON.parse(body);
     var text = store._text;
 
-    if(text !== null) {
+    if(text !== null || body !== undefined) {
       if(text.indexOf("stop") != -1 ) {
         for(j = 0; j < trackPlayers.length; j++ ) {
           trackPlayers[j].stop();
@@ -49,43 +49,3 @@ matrix.on(function(message){
     }, exports.parseResult));
   }
 });
-
-// //if file does not exist, throw false
-// var name = file.load('name.txt');
-
-// //check if name is not empty
-// if(name) {
-//   //if name is not empty, then listen for speech to text, 
-//   //and output when "name" is heard
-//   microphone.record.on('data', function(data) {
-//       if(err) throw err;
-
-//       matrix.notify(out);
-//       //message out on matrix, some sort of namespace for each app
-//   });
-
-//   microphone.info.on('data', function(data){
-//       process.stdout.write(data);
-//   });
-// } else {
-
-//   //say that it needs a name
-//   audio.say('Please log into your mobile application and give me a name');
-
-//   //wait for the name to be entered
-//   controls.on('text.message', function(err, name){
-//     if(err) throw err;
-//     file.save('name.txt', name);
-
-//     //mic.listen == convert speech to text
-//     mic.listen(name).then(function(err, out){
-//       //some sort of namespace for each app
-//       matrix.notify(out);
-//     });
-//   });
-// }
-
-// //listen for events coming in
-// matrix.on('neo.say',function(err, message){
-//     audio.say(message);
-// });
