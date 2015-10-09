f88e248913368da3590f952c1ea5ac698d85a00c
Sean <eighteyes@gmail.com>
2015-10-09 15:37:31 -0400

    Add Base App (NOTE: ALL OLD APPS MIGHT BE BROKEN) Matrix changes
    (bug) Cleaned up application records on stop
    (feature) Make matrix config work

---
+++ apps/base.matrix/index.js
@@ -0,0 +1,7 @@
+matrix = require('./../matrix.js');
+var appName = require('path').basename(__dirname).split('.')[0];
+matrix.name(appName);
+matrix.appConfig = JSON.parse( require('fs').readFileSync(__dirname + '/app.json'));
+matrix.config = matrix.appConfig.configuration;
+
+require('./app.js');
