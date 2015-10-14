5baa65520a7226bbac0ba38dde79e5bae9c7ec17
Sean Canton <eighteyes@gmail.com>
2015-10-14 17:14:41 -0400

    rename

--- apps/base.matrix/index.js
+++ apps/base.matrix/index.js
@@ -1,7 +1,7 @@
 matrix = require('./../matrix.js');
 var appName = require('path').basename(__dirname).split('.')[0];
 matrix.name(appName);
-matrix.appConfig = JSON.parse( require('fs').readFileSync(__dirname + '/app.json'));
+matrix.appConfig = JSON.parse( require('fs').readFileSync(__dirname + '/config.json'));
 matrix.config = matrix.appConfig.configuration;
 
 
