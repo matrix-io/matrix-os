7dcfaee39bddd0868111ebd10e92ef992b50a831
Sean Canton <eighteyes@gmail.com>
2015-10-27 14:56:32 -0400

    Reauthorize on failed token. Comment out clientToken.
    Can probably remove clientToken, left in just in case.

--- app.js
+++ app.js
@@ -67,10 +67,10 @@ async.series([
     // check in with api server
     Matrix.service.token.get(function(err, token){
       if (err) return cb(err);
-      Matrix.token = token.clientToken;
-      Matrix.clientToken = token.clientToken;
+      // Matrix.token = token.clientToken;
+      // Matrix.clientToken = token.clientToken;
       Matrix.deviceToken = token.deviceToken;
-      log('Client Token'.green, token.clientToken);
+      // log('Client Token'.green, token.clientToken);
       log('Device Token'.green, token.deviceToken);
       cb(null);
     });
