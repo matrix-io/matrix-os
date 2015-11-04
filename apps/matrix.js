4692001cb93dcaf6a143df1145b2c64e0bafb0f4
Sean Canton <eighteyes@gmail.com>
2015-11-04 17:53:12 -0500

    Disable open cv, update readme and matrix-hardware

--- apps/matrix.js
+++ apps/matrix.js
@@ -15,7 +15,7 @@ var fs = require('fs');
 var _ = require('lodash');
 var DataStore = require('nedb');
 
-var OpenCv = require('opencv-node-sdk');
+// var OpenCv = require('opencv-node-sdk');
 
 var AppStore =  new DataStore({ filename: config.path.appStore, autoload: true });
 
@@ -168,23 +168,23 @@ function initSensor(name, options, cb) {
 
   if ( name === 'camera' ){
     // pop into OpenCv
-    var eyes = new OpenCv({cameraId:'test'});
-
-    eyes.setConfiguration(options, function(err, a){
-
-      //test
-      var file = require('fs').readFileSync('/Users/god/Desktop/holly.jpg');
-      eyes.analyzeImage(file, function(err, d){
-        console.log('Image', err, d);
-      });
-      console.log('setConfig', err, a)
-      eyes.startCamera(0, function(err, b){
-        console.log('startCamera', err, b);
-        eyes.startContinuousDetection( function(err, c){
-          console.log('detect', err, c);
-        });
-      });
-    });
+    // var eyes = new OpenCv({cameraId:'test'});
+    //
+    // eyes.setConfiguration(options, function(err, a){
+    //
+    //   //test
+    //   var file = require('fs').readFileSync('/Users/god/Desktop/holly.jpg');
+    //   eyes.analyzeImage(file, function(err, d){
+    //     console.log('Image', err, d);
+    //   });
+    //   console.log('setConfig', err, a)
+    //   eyes.startCamera(0, function(err, b){
+    //     console.log('startCamera', err, b);
+    //     eyes.startContinuousDetection( function(err, c){
+    //       console.log('detect', err, c);
+    //     });
+    //   });
+    // });
   }
   // kick off sensor readers
   process.send({
