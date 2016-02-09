1879727c040bd7d8081791d361fc1ad3672e89dd
Sean <eighteyes@gmail.com>
2016-02-09 09:41:00 -0800

    misc fixes for logs

---
+++ apps/testes/app.js
@@ -0,0 +1,33 @@
+var os = require('os');
+var EventEmitter = require('events');
+var emitter = new EventEmitter();
+var cpu = 0;
+var memory = 0;
+
+setTimeout(function() {
+  matrix.send({
+    type: 'device',
+    data: {
+      'os_hostname': os.hostname(),
+      'os_type': os.type(),
+      'os_platform': os.platform(),
+      'os_arch': os.arch()
+    }
+  });
+}, 1000);
+
+setInterval(function() {
+  var loadavg = os.loadavg();
+  var length = loadavg.length;
+  var avg = loadavg.reduce(function(total, num) {
+    return total + num
+  }, 0) / length;
+  var memory = 1 - os.freemem() / os.totalmem();
+  matrix.send({
+    type: 'monitor',
+    data: {
+      'cpu': avg,
+      'memory': memory
+    }
+  });
+}, 500);
