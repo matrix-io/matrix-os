2018254e035b3131e9a2e1461f71fb97adbf084e
Sean Canton <eighteyes@gmail.com>
2016-04-11 11:34:53 -0700

    Add configuration v2 and tests

---
+++ test/config.test.js
@@ -0,0 +1,10 @@
+describe('Configuration via Firebase', function(){
+
+  
+  it('should save an application configuration');
+  it('should update a configuration based on local config.yaml');
+  it('should provide a configuration to an application');
+  it('should emit a `config-update` when config is updated');
+  it('should restart an application when it\'s active configuration is changed');
+  it('should assign an application to a device in the configuration');
+})
