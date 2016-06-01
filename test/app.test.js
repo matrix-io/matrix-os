a32f4cf16edfb871b23b01e353aea49047623d73
Sean Canton <eighteyes@gmail.com>
2016-06-01 12:10:31 -0700

    add test

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,14 @@
 
 
 describe('Matrix Applications', function(){
+  before(function(){
+    require('child_process').execSync('cp -r '+ __dirname + '/test.matrix/ '+ __dirname +'/../apps/test.matrix/');
+    app = new Matrix.service.application.Application('test');
+  })
+
+  after(function(){
+    require('child_process').execSync('rm -r '+__dirname+'/../apps/test.matrix');
+  })
   describe('Lifecycle', function(){
     it('should be able to install an app')
     it('should be able to update an app')
@@ -10,16 +18,24 @@ describe('Matrix Applications', function(){
     describe('activeApplications', function(){
       var appRecord;
       before( function(){
-        appRecord = _.filter( Matrix.activeApplications, { name: 'test '});
+        appRecord = _.filter( Matrix.activeApplications, { name: 'test'});
       })
       it('should save a reference to the name', function(done){
-        appRecord.length.should.be(1);
+        appRecord.length.should.equal(1);
+        done();
       })
       it('should save a reference to the process', function(done){
-        appRecord.should.have.property('process')
+        appRecord.should.have.property('process');
+        done();
       })
-      it('should save a reference to the configuration')
-      it('should save a reference to the sensors')
+      it('should save a reference to the configuration', function (done) {
+        appRecord.should.have.property('config');
+        done();
+      })
+      it('should save a reference to the sensors', function (done) {
+        appRecord.should.have.property('sensors');
+        done();
+      })
     })
     it('should stop an app by name');
     it('should stop all apps');
