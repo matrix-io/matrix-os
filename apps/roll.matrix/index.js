6730f3dd6eae59f0f64f48b404cc88c036ba5e33
Sean Canton <eighteyes@gmail.com>
2016-09-27 10:45:37 -0700

    Remove old apps

--- apps/roll.matrix/index.js
+++
@@ -1,9 +0,0 @@
-var appName = require('path').basename(__dirname).split('.')[0];
-
-matrix = require('./../matrix.js')
-
-matrix.startApp(appName);
-
-matrix.ready(function(){
-  require('./app.js');  
-})
