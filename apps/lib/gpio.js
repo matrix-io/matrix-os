810b44b4e685e387039384b1cc54b7983fe6fab8
Diego Ribero <diego.ribero@admobilize.com>
2016-12-02 16:35:35 -0500

    Working on GPIO WRITE

--- apps/lib/gpio.js
+++ apps/lib/gpio.js
@@ -1,11 +1,45 @@
 
 // pin # = index
-var pinHandlers = []; //Callback for each pin 
+var pinHandlers = []; //Callback for each pin
+var pinWriteHandlers = []; //Callback for each pin write
 var readPins = []; //Pins that are being notified
 var writePins = [];
 
 var listenerStarted = false;
 
+function setListener() { 
+  if (!listenerStarted) { //Only start listening once
+    listenerStarted = true;
+    process.on('message', function (message) {
+      if (message.eventType == 'gpio-emit') {
+
+        if (message.payload.type == 'read') {
+          var resultPins = message.payload.values;
+          readPins.forEach(function (element) {
+            var value = 0;
+            if (resultPins.hasOwnProperty(element)) { //Only assign a value to that pin if a value is specified 
+              value = resultPins[element];
+            }
+            pinHandlers[element](value);
+          }); 
+          
+        } else if (message.payload.type == 'write') {
+          console.log('WRITE LISTENER!:', message);
+          /*var resultPins = message.payload.values;
+          readPins.forEach(function (element) {
+            var value = 0;
+            if (resultPins.hasOwnProperty(element)) { //Only assign a value to that pin if a value is specified 
+              value = resultPins[element];
+            }
+            pinHandlers[element](value);
+          });
+          */
+          //pinWriteHandlers[element];
+        }
+      }
+    })
+  };
+};
 
 module.exports = {
   read: function (pin, cb) {
@@ -22,29 +56,19 @@ module.exports = {
       pin: pin
     });
 
-    if (!listenerStarted) { //Only start listening once
-      listenerStarted = true;
-      process.on('message', function (message) {
-        if (message.eventType == 'gpio-emit') {
-          var resultPins = message.payload.values;
-          readPins.forEach(function (element) {
-            var value = 0;
-            if (resultPins.hasOwnProperty(element)) { //Only assign a value to that pin if a value is specified 
-              value = resultPins[element];
-            }
-            pinHandlers[element](value);
-          });
-        }
-      })
-    }    
+    setListener();
   },
 
   write: function (pin, value, cb) {
     console.log("PIN WRITE: ", pin, value);
-    process.send('gpio-write', { pin: pin, value: value });
-    if (_.isFunction(cb)) {
-      cb();
-    }
+    pinWriteHandlers[pin] = cb;
+    process.send({
+      type: 'gpio-write',
+      pin: pin,
+      value: value
+    });
+    //process.send('gpio-write', { pin: pin, value: value });
+    setListener();
   },
   open: function () {
     
