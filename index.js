b30a99cd69a3011ccdcaaab78c68f4109d591468
Sean Canton <eighteyes@gmail.com>
2017-02-01 14:09:24 -0800

    Refactor Zigbee Command, add awesome 💥

--- index.js
+++ index.js
@@ -442,6 +442,7 @@ var msg = [];
       log('MXSS Connected:'.green, Matrix.streamingServer.grey)
     }
 
+    Matrix.device.drivers.zigbee.toggle();
     // Show whats available from MALOS
 
     if ( malosInfoOut.length > 0 ){
