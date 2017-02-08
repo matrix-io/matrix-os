42f08b651701d390efacd58ae8471b03d72cb5a9
Sean Canton <eighteyes@gmail.com>
2017-02-08 11:18:51 -0800

    Wire up train and recognize

--- apps/recog.matrix/app.js
+++ apps/recog.matrix/app.js
@@ -2,3 +2,9 @@
 // matrix.init()....
 //
 // have fun
+matrix.on('train', function(){
+  console.log('training started>>>>>')
+  matrix.train('test').then(function(d){
+    console.log('trained!', d)
+  })
+})
