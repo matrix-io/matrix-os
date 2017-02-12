b854b95884861544eb549ccd84dfa5301260868c
Sean Canton <eighteyes@gmail.com>
2017-02-11 19:38:51 -0800

    Wired it up. No connectivity to servie yet

--- apps/recog.matrix/app.js
+++ apps/recog.matrix/app.js
@@ -10,3 +10,10 @@ matrix.on('train', function(d){
     console.log('trained!', d);
   });
 });
+
+matrix.on('recog', function(d){
+  console.log('recog!');
+  matrix.init('recognition', {tag: 'test'}).then(function (d) {
+    console.log('RECOGNIZED!!>!!>!>>!>!>!>', d);
+  })
+})
