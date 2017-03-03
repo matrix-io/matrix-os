f4b492281f939909870201a2df44db4a81b82ff5
Sean Canton <eighteyes@gmail.com>
2017-03-03 11:01:39 -0800

    Slight LED changes and UPDATE MOS API

--- apps/recog.matrix/app.js
+++ apps/recog.matrix/app.js
@@ -1,4 +1,31 @@
+// lighting
+var a = 180;
+var a2 = 0;
+var l = setInterval(function() {
+  matrix.led([{
+    arc: Math.round(145 * Math.sin(a)),
+    color: 'green',
+    start: a2
+  }, {
+    arc: -Math.round(145 * Math.sin(a)),
+    color: 'blue',
+    start: a2 + 180
+  }]).render();
+  a = (a < 0) ? 180 : a - 0.1;
+  a2 = (a2 > 360) ? 0 : a2 + 1;
+}, 25);
+
+function stopLights() {
+  clearInterval(l);
+}
+
+
+
+
+
+
 matrix.on('reset', function() {
+  stopLights();
   matrix.service('recognition').untrain('test')
   matrix.led('red').render()
   setTimeout(function() {
@@ -6,13 +33,19 @@ matrix.on('reset', function() {
   }, 1000)
 })
 
+
+
 matrix.on('listtag', function() {
+
+  stopLights();
   matrix.service('recognition').getTags().then(function(tags) {
     console.log('>>>>>>>>>>TAGS>>>>>>>>>>>>>>', tags);
   })
 })
 
 matrix.on('train', function(d) {
+
+  stopLights();
   var trained = false;
   console.log('training started>>>>>', d);
   matrix.service('recognition').train('test').then(function(d) {
@@ -36,6 +69,8 @@ matrix.on('train', function(d) {
 // })
 
 matrix.on('recog', function(d) {
+
+  stopLights();
   matrix.init('recognition', { tag: 'test' }).then(function(d) {
     console.log('RECOG>>>!', d);
     matrix.led('green').render();
