a9f06ab3c6422ad8af682d94b488257b7022afd3
AlvaroArdila <alvaro.ardila@admobilize.com>
2017-09-18 15:48:11 -0500

    finish bin test

--- test/bin.test.js
+++ test/bin.test.js
@@ -3,9 +3,11 @@ var binPath = './bin/';
 var exec = require('child_process').exec;
 var DataStore = require('nedb');
 var _ = require('lodash');
-var config = require(__dirname + '/../config/path');
+var path = require('path');
+var root = path.join(__dirname,'../');
+var deviceDbPath = root + 'db/device.db'
 var deviceDB = new DataStore({
-  filename: config.db.device,
+  filename: deviceDbPath,
   autoload: true
 });
 
@@ -29,58 +31,37 @@ describe('Bin', function () {
       }
     });
 
-    // after(function () {
-    //   var bkFileStat, err;
-    //   //Restore Backup and remove File      
-    //   try {
-    //     bkFileStat = fs.statSync(deviceBackupFilePath);
-    //   } catch (error) {
-    //     err = error;
-    //   }
-    //   if (!err && bkFileStat) {
-    //     fs.copySync(deviceBackupFilePath, deviceFilePath);
-    //     fs.unlinkSync(deviceBackupFilePath);
-    //   }
-    // });
+    after(function () {
+      var bkFileStat, err;
+      //Restore Backup and remove File      
+      try {
+        bkFileStat = fs.statSync(deviceBackupFilePath);
+      } catch (error) {
+        err = error;
+      }
+      if (!err && bkFileStat) {
+        fs.copySync(deviceBackupFilePath, deviceFilePath);
+        fs.unlinkSync(deviceBackupFilePath);
+      }
+    });
     
     describe('Reset device', function () {
       it('should reset the configuration for all environments', function (done) {
         exec('node ' + binPath + 'reset.js', function (error, stdout, stderr) {
           if (!error) {
-            var query = {};
-            deviceDB.find(query, function (err, result) {
-              if (err) {
-                done(err);
-              }
-              //check if there is any data left for any envs
-              if (_.isUndefined(result)) {
-                done();
-              } else {
-                done(new Error('Reset failed'));
-              }
-            });
-          }else{
-            done(error);
-          }
-        }); 
-      });
-
-      var env = 'dev';
-
-      it('should reset the configuration for a specific environment', function (done) {
-        exec( 'node ' + binPath + 'reset.js ' + env, function (error, stdout, stderr) {
-          if (!error) {
-            var query = {};
-            deviceDB.find(query, function (err, result) {
-              if (err) {
-                done(err);
-              }
-              //check if there is any data left for env
-              if (_.isUndefined(result)) {
-                done();
-              } else {
-                done(error);
-              }
+            deviceDB.loadDatabase(function (err) { 
+              var query = { env: { $exists: true } };
+              deviceDB.find(query, function (err, result) {
+                if (err) {
+                  done(err);
+                }
+                //check if there is any data left for any envs
+                if (result.length === 0) {
+                  done();
+                } else {
+                  done(new Error('Reset failed'));
+                }
+              });
             });
           }else{
             done(error);
@@ -90,29 +71,50 @@ describe('Bin', function () {
     });
       
     describe('Set device', function () {
-      before(function () {
-        //TODO Remove previous data for env
-      });
       var env = 'dev';
       var id = 'ThisIsMyTestId';
       var secret = 'DeviceSecretAtItsBest';
 
-      describe.skip('Correct command', function () {
+      describe('Correct command', function () {
         it('should be able to set the device configuration for a specific environment', function (done) {
           exec( 'node ' + binPath + 'set.js ' + env + ' ' + id + ' ' + secret, function (error, stdout, stderr) {
             if (!error) {
-              var query = {};
-              deviceDB.find(query, function (err, result) {
-                if (err) {
-                  done(err);
-                }
+              deviceDB.loadDatabase(function (err) { 
+                var query = {'env': env};
+                deviceDB.find(query, function (err, result) {
+                  if (err) {
+                    done(err);
+                  }
+                  //Use NeDB find to check if there's data for that environment
+                  if (result.length != 0) {
+                    done();
+                  } else {
+                    done(new Error('Set device failed'));
+                  }
+                });
+              });
+            }else{
+              done(error);
+            }
+          }); 
+        });
 
-                //Use NeDB find to check if there's data for that environment
-                if (_.isUndefined(result)) {
-                  done();
-                } else {
-                  done(error);
-                }
+        it('should reset the configuration for a specific environment', function (done) {
+          exec( 'node ' + binPath + 'reset.js ' + env, function (error, stdout, stderr) {
+            if (!error) {
+              var query = {'env': env};
+              deviceDB.loadDatabase(function (err) { 
+                deviceDB.find(query, function (err, result) {
+                  if (err) {
+                    done(err);
+                  }
+                  //check if there is any data left for env
+                  if (result.length === 0) {
+                    done();
+                  } else {
+                    done(new Error('Reset the configuration for a specific environment failed'));
+                  }
+                });
               });
             }else{
               done(error);
@@ -122,14 +124,10 @@ describe('Bin', function () {
       });
       
       describe('Wrong parameters', function () {
-        before(function () { 
-          //TODO Remove previous data for env
-        });
-
         it('should not set device configuration if env isn\'t dev|rc|prod', function (done) {
           exec('node ' + binPath + 'set.js ' + 'aMadeUpEnv' + ' ' + id + ' ' + secret, function (error, stdout, stderr) {
             if (!error) {
-              done(error);
+              done(new Error('Wrong parameters validation failed'));
             }else{
               done();
             }
@@ -139,7 +137,7 @@ describe('Bin', function () {
         it('should not set device configuration if missing env parameter', function (done) {
           exec('node ' + binPath + 'set.js ' + id + ' ' + secret, function (error, stdout, stderr) {
             if (!error) {
-              done(error);
+              done(new Error('Wrong parameters validation failed'));
             }else{
               done();
             }
@@ -149,7 +147,7 @@ describe('Bin', function () {
         it('should not set device configuration if missing device id', function (done) {
           exec('node ' + binPath + 'set.js ' + env + ' ' + secret, function (error, stdout, stderr) {
             if (!error) {
-              done(error);
+              done(new Error('Wrong parameters validation failed'));
             }else{
               done();
             }
@@ -159,7 +157,7 @@ describe('Bin', function () {
         it('should not set device configuration if missing device secret', function (done) {
           exec( 'node ' + binPath + 'set.js ' + env + ' ' + id, function (error, stdout, stderr) {
             if (!error) {
-              done(error);
+              done(new Error('Wrong parameters validation failed'));
             }else{
               done();
             }
@@ -169,13 +167,12 @@ describe('Bin', function () {
         it('should not set device configuration if missing multiple parameters', function (done) {
           exec('node ' + binPath + 'set.js', function (error, stdout, stderr) {
             if (!error) {
-              done(error);
+              done(new Error('Wrong parameters validation failed'));
             }else{
               done();
             }
           }); 
         });
-
       });
     });
   });
