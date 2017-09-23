c7ade9ebed9872dc3a39f14463f7df5324e6921a
Sean Canton <eighteyes@gmail.com>
2017-09-22 17:13:05 -0700

    more progress for protobuf, finally testing

--- apps/lib/led.js
+++ apps/lib/led.js
@@ -28,7 +28,7 @@ module.exports = function(c) {
     var tcColors = _.times('#000000', 35);
 
     if (_.isPlainObject(color)) {
-      // console.log('o>', color)
+      // console.log('o>', color);
       if ( color.color.indexOf('rgba') !== -1 ){
         console.log('rgba is not supported. results may vary.');
       }
