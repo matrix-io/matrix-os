961b25df758f57d098f2e95fe71577b706ab6a62
Maciek Ruckgaber <maciekrb@gmail.com>
2017-10-04 01:41:31 -0500

    Updated package with matrixio prefix + cleanup scripts

--- debian/util/cleanup.sh
+++ debian/util/cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo systemctl stop matrix-os.service 
-sudo systemctl disable matrix-os.service
-sudo rm /lib/systemd/system/matrix-os.service
+sudo systemctl stop matrixio-os.service 
+sudo systemctl disable matrixio-os.service
+sudo rm /lib/systemd/system/matrixio-os.service
 sudo rm -rf /var/matrix-os/ /usr/lib/matrix-os/
-sudo userdel -r matrix-os
+sudo userdel -r matrixio-os
